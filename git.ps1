94c45995534a234fce6324b99963df8840691ef8
ComFreek <comfreek@outlook.com>
2021-02-09 18:55:40 +0100

    fix git aliasses for commas at cli

--- git.ps1
+++ git.ps1
@@ -1,3 +1,22 @@
+<#
+
+Fully flattens an array.
+
+Source: <https://stackoverflow.com/a/33545660/603003>
+Author: Matt <https://stackoverflow.com/users/3829407/matt>
+License: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
+
+#>
+function Flatten-Array {
+  $input | ForEach-Object{
+      if ($_ -is [array]){$_ | Flatten-Array}else{$_}
+  } | Where-Object{![string]::IsNullorEmpty($_)}
+}
+
+function Flatten-Args {
+	($input | Flatten-Array) -join " "
+}
+
 function gs {
 	& git status
 }
@@ -27,18 +46,18 @@ function gpu {
 # commit all ("ca") in a git repo
 function ca {
 	& git add --all
-	& git commit -m $($args -join " ")
+	& git commit -m $($args | Flatten-Args)
 	& git status
 }
 
 # commit all ammend ("caa") in a git repo
 function caa {
-	& git commit --all --amend -m $($args -join " ")
+	& git commit --all --amend -m $($args | Flatten-Args)
 	& git status
 }
 
 function cas {
-	& git commit --all -m $(($args -join " ") + " [skip ci]")
+	& git commit --all -m $(($args | Flatten-Args) + " [skip ci]")
 	& git status
 }
 
